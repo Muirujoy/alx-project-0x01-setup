d93f4cd9a2d987b6989f9698b400a0856fff0a9f
Joy <muirujoybackup@gmail.com>
2025-07-18 15:42:35 +0300

    added

--- alx-project-0x01/components/common/UserCard.tsx
+++ alx-project-0x01/components/common/UserCard.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { UserProps } from "@/interfaces";
 
-const UserCard: React.FC<{ user: UserProps }> = ({ user }) => {
+const UserCard: React.FC<UserProps> = ({ name, username, email, address, company }) => {
   return (
     <div className="bg-white shadow-lg p-4 rounded-lg">
-      <h2 className="text-lg font-semibold">{user.name}</h2>
-      <p className="text-sm text-gray-600">@{user.username}</p>
-      <p className="text-sm">{user.email}</p>
+      <h2 className="text-lg font-semibold">{name}</h2>
+      <p className="text-sm text-gray-600">@{username}</p>
+      <p className="text-sm">{email}</p>
       <p className="text-sm">
-        {user.address.city}, {user.address.street}
+        {address.city}, {address.street}
       </p>
-      <p className="text-sm">{user.company.name}</p>
+      <p className="text-sm">{company.name}</p>
     </div>
   );
 };
