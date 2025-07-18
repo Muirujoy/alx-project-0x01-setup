b90f312c43525718d73f0ca49e6a9d400cb5d914
Joy <muirujoybackup@gmail.com>
2025-07-18 15:07:37 +0300

    added

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 text-center p-4 mt-auto">
+      <p>&copy; 2025 ALX Project</p>
+    </footer>
+  );
+};
+
+export default Footer;
